7e8983206e719572eafe14347e3b95d6c1fc7cea
JuniorJose-git <joseronaldosantus@gmail.com>
2026-05-27 19:25:28 -0300

    .

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ errors=()
 find . -name "*.puml" | while read -r file; do
   dir="$OUT/$(dirname "$file" | sed 's|^\./||')"
   mkdir -p "$dir"
-  if ! plantuml -tsvg -o "$(realpath "$dir")" "$file" 2>/dev/null; then
+  if ! plantuml -o "$(realpath "$dir")" "$file" 2>/dev/null; then
     echo "WARN: failed to generate $file"
   fi
 done
